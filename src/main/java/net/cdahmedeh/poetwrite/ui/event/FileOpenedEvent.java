836170ec184509271e9900f6973e0dadf3c70da6
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2026-07-10 20:11:27 -0400

    Rename persistence to manager.

--- src/main/java/net/cdahmedeh/poetwrite/ui/event/FileOpenedEvent.java
+++ src/main/java/net/cdahmedeh/poetwrite/ui/event/FileOpenedEvent.java
@@ -18,16 +18,14 @@
 
 package net.cdahmedeh.poetwrite.ui.event;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import net.cdahmedeh.poetwrite.ui.app.PersistenceHandler;
+import net.cdahmedeh.poetwrite.ui.app.PersistenceManager;
 
 @NoArgsConstructor
 public class FileOpenedEvent extends FileEvent {
 
     @Override
-    public PersistenceHandler.FileStatus getFileStatus() {
-        return PersistenceHandler.FileStatus.OPENED;
+    public PersistenceManager.FileStatus getFileStatus() {
+        return PersistenceManager.FileStatus.OPENED;
     }
 }
