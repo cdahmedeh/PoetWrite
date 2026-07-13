955d99db923b9b1f13ce4ccb091daeffb8e8b5d4
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2026-07-13 13:27:22 -0400

    Added annotation for duplicated code.

---
+++ src/main/java/net/cdahmedeh/poetwrite/annotation/Duplicated.java
@@ -0,0 +1,9 @@
+package net.cdahmedeh.poetwrite.annotation;
+
+/**
+ * Just to remind myself in case there is duplicated code. Such as when
+ * different view controllers have the same code.
+ */
+public @interface Duplicated {
+    public String value() default "";
+}
