b178223ea7cff060742a2d46e3255e0c0342a377
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2026-09-03 16:02:39 -0400

    Doc for autocomplete tree holder.

--- src/main/java/net/cdahmedeh/poetwrite/query/holder/AutoCompleteTreeHolder.java
+++ src/main/java/net/cdahmedeh/poetwrite/query/holder/AutoCompleteTreeHolder.java
@@ -1,3 +1,21 @@
+/**
+ * PoetWrite - A Poetry Writing Application
+ * Copyright (C) 2026 Ahmed El-Hajjar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package net.cdahmedeh.poetwrite.query.holder;
 
 import net.cdahmedeh.poetwrite.query.interfaces.QueryStep;
@@ -17,6 +35,9 @@ import javax.inject.Singleton;
  *       rebuilt by getSteps() each time so they start clean, but these two do
  *       not. Either rebuild per request, or clear the parameters here, once
  *       it is clear which the wizard wants.
+ *
+ *  TODO: tree() hangs on the first time, and then re-used. Should look into
+ *        using the cache that is used accross the board.
  */
 @Singleton
 public class AutoCompleteTreeHolder {
@@ -30,7 +51,6 @@ public class AutoCompleteTreeHolder {
         this.rootQueryStep = rootQueryStep;
     }
 
-    /** Blocking on the first call. Built once, then handed back. */
     public QueryStep tree() {
         if (tree == null) {
             tree = rootQueryStep.build();
