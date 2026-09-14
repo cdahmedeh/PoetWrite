bc4b3c62f30ae4536eba7ba09cf1c753f19be1f7
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2026-09-14 13:24:51 -0400

    Editor Status Service for keeping track of what is being done in the editor.

--- src/main/java/net/cdahmedeh/poetwrite/ui/services/EditorStatusHolder.java
+++ src/main/java/net/cdahmedeh/poetwrite/ui/services/EditorStatusHolder.java
@@ -18,8 +18,67 @@
 
 package net.cdahmedeh.poetwrite.ui.services;
 
-import net.cdahmedeh.poetwrite.lib.analysis.PoemAnalysis;
+import lombok.Getter;
 import net.cdahmedeh.poetwrite.lib.domain.Poem;
+import net.cdahmedeh.poetwrite.service.interfaces.LazyService;
+import net.cdahmedeh.poetwrite.ui.async.TaskBus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class EditorStatusHolder {
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Keeps track of what is being done in the editor, such as the current line
+ * that the user is selecting based on cursor location.
+ *
+ * Mostly used by the assisted queries. For example, for rhyming, switching
+ * between the previous and next line.
+ */
+@Singleton
+public class EditorStatusHolder extends LazyService {
+    private static final Logger LOG = LoggerFactory.getLogger(EditorStatusHolder.class);
+
+    // The parsed Poem object structure.
+    @Getter
+    private Poem poem;
+
+    // The line selected by the cursor.
+    // NOTE: INDEX STARTS AS 1 FOR FIRST LINE.
+    @Getter
+    private int currentLine = -1;
+
+    @Inject
+    protected EditorStatusHolder(TaskBus taskBus) {
+        super(taskBus);
+    }
+
+    @Override
+    public String name() {
+        return "Editor Status";
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    /**
+     * Called after the Poem has been parsed.
+     */
+    public void poem(Poem poem) {
+        this.poem = poem;
+    }
+
+    /**
+     * Called as soon as the user moves the cursor.
+     */
+    public void update(int line) {
+        currentLine = line;
+        LOG.debug("Editor Line Changed: {}", line);
+
+        if (poem == null) {
+            LOG.error("Poem is null");
+        }
+    }
 }
